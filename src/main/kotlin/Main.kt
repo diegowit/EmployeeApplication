e56145a0882c42d4677eafc699c13f86e9993bac
diego <20098757@mail.wit.ie>
2023-10-07 00:49:55 +0100

    the employeeMenu structure is added With no functions Implemented, it has 5 options. 1. add 2. list 3. paySlip 4. update 5. delete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,9 +11,9 @@ fun main() = runMainMenu()
 fun runMainMenu() {
     do {
         when (val option = mainMenu()) {
-            //1 -> runEmployeeMenu()
-           // 2 -> runEmployeeSearch()
-           // 0 -> exitApp()
+            1 -> runEmployeeMenu()
+            2 -> runEmployeeSearch()
+            0 -> exitApp()
             else -> println("Invalid menu choice: $option")
         }
     } while (true)
@@ -49,6 +49,57 @@ fun mainMenu(): Int {
 ║                  0    Exit                        ║
 ╚═══════════════════════════════════════════════════╝
 ==>> """.trimMargin()
+    )
+    return readLine()!!.toInt()
+    // Returning the user input as an integer.
+}
+
+/**
+ *
+ *  Employee Menu
+ *
+ */
+
+fun runEmployeeMenu () {
+
+    do {
+        when (val option = employeeMenu()) {
+          //  1 -> add()
+           // 2 -> list()
+           // 3 -> paySlip()
+           // 4 -> update()
+          //  5 -> delete()
+
+            //20 -> save()
+           // 21 -> load()
+            0 -> return // Return to main menu
+            else -> println("Invalid menu choice: $option")
+        }
+    } while (true)
+}
+
+fun employeeMenu(): Int {
+
+    print(
+        """
+╔═══════════════════════════════════════════════════╗
+║                   Employee Menu                   ║
+╠═══════════════════════════════════════════════════╣
+║   1) Add a Employee                               ║
+║   2) List All Employee                            ║
+║   3) Print Payslip for Employee                   ║
+║   4) Update Employee details                      ║
+║   5) Delete Employee                              ║
+║                                                   ║
+╟───────────────────────────────────────────────────╢
+║   20) Save Contacts                               ║
+║   21) Load Contacts                               ║
+╚═══════════════════════════════════════════════════╝
+║   0) Return to Main Menu                          ║
+╚═══════════════════════════════════════════════════╝
+            
+            
+       ==>> """.trimMargin()
     )
     return readLine()!!.toInt()
     // Returning the user input as an integer.
