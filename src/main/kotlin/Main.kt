5dac971000a7e862c469ef2532a11265473e5842
diego <20098757@mail.wit.ie>
2023-10-07 00:27:21 +0100

    the main menu structure is added with 3 options. 1. runEmployeeMenu 2. RunEmployeeSearch 3.ExitApp

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,31 @@
 package main
+import main.models.Employee
 
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+
+/**
+ * Runs the main menu loop.
+ */
+// Function to display the menu options and return the user input.
+fun runMainMenu() {
+    do {
+        when (val option = mainMenu()) {
+            1 -> runEmployeeMenu()
+            2 -> runEmployeeSearch()
+            0 -> exitApp()
+            else -> println("Invalid menu choice: $option")
+        }
+    } while (true)
+}
+
+/**
+ *
+ *  Main Menu
+ *
+ */
+fun mainMenu(args: Array<String>){
+    //start()
+    // logger.info { "Launching Employee App" }
+
 }
