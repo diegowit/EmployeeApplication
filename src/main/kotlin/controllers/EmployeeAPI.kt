b759a0fa585818dbac3716f0c54c48b61fef8ae9
diego <20098757@mail.wit.ie>
2023-10-07 01:29:08 +0100

    a variable to store the last used employee ID and a Function to generate unique employee IDs: getId() added in the EmployeeAPI.kt

--- src/main/kotlin/controllers/EmployeeAPI.kt
+++ src/main/kotlin/controllers/EmployeeAPI.kt
@@ -2,6 +2,15 @@ package main.controllers
 
 import main.models.Employee
 
+// Initialize a variable to store the last used employee ID.
+var lastId = 0
+
+// Function to generate unique employee IDs.
+internal fun getId(): Int {
+    // Increment the last used ID and return the result.
+    return lastId++
+}
+
 class EmployeeAPI{
 
     // Collection to store employee instances.
